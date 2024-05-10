77c9783f132ab054c221237155bd93e8a79888d4
rayalva407 <rayalva407@gmail.com>
2024-05-10 10:45:26 -0600

    implements Enumerable#my_select

--- lib/my_enumerables.rb
+++ lib/my_enumerables.rb
@@ -8,6 +8,18 @@ module Enumerable
 
     self
   end
+
+  def my_select
+    result = []
+
+    if block_given?
+      self.my_each do |element|
+        result << element if yield(element)
+      end
+    end
+
+    result
+  end
 end
 
 # You will first have to define my_each
