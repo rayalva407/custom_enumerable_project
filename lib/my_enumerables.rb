36fb0a7d449eac59cea8ae674ed487fbefacd00f
rayalva407 <rayalva407@gmail.com>
2024-05-10 10:14:18 -0600

    implements Array#my_each

--- lib/my_enumerables.rb
+++ lib/my_enumerables.rb
@@ -7,5 +7,13 @@ end
 # your enumerable module will have access
 # to this method
 class Array
-  # Define my_each here
+  def my_each
+    if block_given?
+      (0...self.length).each { |i|
+        yield(self[i])
+      }
+    end
+
+    self
+  end
 end
